75ee54eeabcdad3e1b75aa135952abdb2a728e43
RUnternahrer <ryan@ryanunternahrer.me>
2024-10-27 11:29:37 +1300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module me.ryanunternahrer.weather {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens me.ryanunternahrer.weather to javafx.fxml;
+    exports me.ryanunternahrer.weather;
+}
